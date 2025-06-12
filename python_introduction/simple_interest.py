98e54f019578ff6b0d5969e5d31efbf0cced1d13
Rachel E. Egbe <rachelegbe13@gmail.com>
2025-06-12 15:34:14 +0100

    Add simple_interest.py to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#Declaring the variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#The formula for calculating interest
+interest = principal * rate * time
+
+print ("The simple interest is:", interest)
